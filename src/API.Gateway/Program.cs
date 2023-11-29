e86b75b3b54408dc6566dfb45f72ce5728febd43
Radek Jakubowski <jakubowskiumieszcz@gmail.com>
2023-11-29 11:10:20 +0100

    reverse proxy seems to route accordingly

--- src/API.Gateway/Program.cs
+++ src/API.Gateway/Program.cs
@@ -1,5 +1,8 @@
+using AuthProvider;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDefaultAuthentication(builder.Configuration);
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetRequiredSection("ServicesRoutes"));
 
 var app = builder.Build();
---
+++ src/AuthProvider/AuthProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthProvider;
+
+public static class AuthProvider
+{
+  public static void AddDefaultAuthentication(this IServiceCollection services, ConfigurationManager configuration)
+  {
+    services.AddAuthentication(opts =>
+       {
+         opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+         opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+       })
+       .AddJwtBearer(opts =>
+       {
+         opts.RequireHttpsMetadata = false;
+         opts.TokenValidationParameters = new TokenValidationParameters
+         {
+           ValidIssuer = configuration["JWT:Issuer"],
+           ValidateIssuer = false,
+           IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+           ValidateAudience = false,
+           ValidAudience = configuration["JWT:Issuer"]
+         };
+       });
+  }
+}
+
