7ade783ff95202a59014d03523c500c177312f35
Radek Jakubowski <jakubowskiumieszcz@gmail.com>
2023-12-01 12:21:22 +0100

    migrations and connection to sql db - onto automigration on launch and testing reverse proxy

--- src/UsersAPI/Infrastructure/ApplicationUser.cs
+++ src/UsersAPI/Infrastructure/ApplicationUser.cs
@@ -7,7 +7,6 @@ public class ApplicationUser : IdentityUser
   public string? EmailConfirmationToken { get; set; }
   public string? RefreshToken { get; set; }
   public DateTime? RefreshTokenValidTo { get; set; }
-  public DateTime? LastVoteDate { get; set; }
   public string? PasswordResetToken { get; set; }
   public DateTime? PasswordResetTokenValidTo { get; set; }
 }
